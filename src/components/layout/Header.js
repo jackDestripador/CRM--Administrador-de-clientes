60f16baa8c9b36cb21e19ddecc11732c3360277d
jesus <chucho.cisneros@hotmail.com>
2021-06-21 15:24:56 -0500

    Inicio del proyecto

---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header className="barra">
+    <div className="contenedor">
+      <h1>CRM -Administrador de clientes</h1>
+    </div>
+  </header>
+);
+
+export default Header;
